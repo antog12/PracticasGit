e9cf2f782ee4c54c75b2f02fbe7b7736f3574e81
Antonio angel <aabellan@ext.laligatech.com>
2023-02-03 10:23:37 +0100

    Párrafo añadido

---
+++ FuncionesPython/Funciones.py
@@ -0,0 +1,10 @@
+print("---------------------")
+
+def sumar(param1,param2):
+    return param1 + param2
+num1= int(input("introduce el primer num: "))
+num2= int(input("introduce el segundo num: "))
+resultado= sumar(num1,num2)
+print("El resultado de la suma es: ",resultado)
+
+print("---------------------")
